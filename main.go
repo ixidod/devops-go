49b32b23c9d5039b7f59f41944d80a8eb8cd322c
Patrycjusz Myszka <pat.myszka@gmail.com>
2025-02-02 20:02:05 +0000

    change in argocd to sync automate

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "teraz wersja finalna caly pipeline ci/cd dziala jak ta llala")
+	fmt.Fprintln(w, " lekka zmiana w auto-sync argocd teraz wersja finalna caly pipeline ci/cd dziala jak ta llala")
 }
 func main() {
 	http.HandleFunc("/", handler)
